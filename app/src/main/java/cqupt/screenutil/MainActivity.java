22778661bd39d75fbf41cb911d39baf443ba0cb9
Coco001 <wsgkk000@163.com>
2017-02-18 10:16:47 +0800

    访问手机中的图库或者相机获取照片

--- app/src/main/java/cqupt/screenutil/MainActivity.java
+++ app/src/main/java/cqupt/screenutil/MainActivity.java
@@ -1,17 +1,112 @@
 package cqupt.screenutil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 返回码：系统图库
+    private static final int CODE_IMAGE = 100;
+    // 返回码：相机
+    private static final int CODE_CAMERA = 200;
+    // IMAGE TYPE
+    private static final String IMAGE_TYPE = "image/*";
+    // Temp照片路径
+    public static String TEMP_IMAGE_PATH = Environment.getExternalStorageDirectory().getPath() + "/temp.png";
+    // 本地图册、相机选择
+    private String[] mCustomItems = new String[]{"本地图册", "相机拍照"};
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, ScreenUtil.getScreenSize(this) + "");
-        Log.d(TAG, ScreenUtil.getDeviceDensity(this) + "");
     }
+    // 显示选择系统图库 相机对话框
+    private void showDialogCustom() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("选择：");
+        builder.setItems(mCustomItems, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (0 == which) {// 本地图册
+                    //运行时权限检查
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(MainActivity.this,
+                            READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Intent intent = new Intent(Intent.ACTION_PICK, null);
+                        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_TYPE);
+                        startActivityForResult(intent, CODE_IMAGE);
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    }
+                } else if (1 == which) {// 系统相机
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    Uri photoUri = Uri.fromFile(new File(TEMP_IMAGE_PATH));
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(intent, CODE_CAMERA);
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, null);
+                    intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_TYPE);
+                    startActivityForResult(intent, CODE_IMAGE);
+                } else {
+                    Toast.makeText(MainActivity.this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 调用图库相机回调方法
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CODE_IMAGE && data != null) {
+                // 相册
+                Cursor cursor = this.getContentResolver().query(data.getData(), null, null, null, null);
+                cursor.moveToFirst();
+                String imagePath = cursor.getString(cursor.getColumnIndex("_data"));
+
+                cursor.close();
+            } else if (requestCode == CODE_CAMERA) {
+                // 相机
+                // 照片保存在了 TEMP_IMAGE_PATH 路径下，通过该路径即可获取照片
+            }
+        }
+    }
+
 }
