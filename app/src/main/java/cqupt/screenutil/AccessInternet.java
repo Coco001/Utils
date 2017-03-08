d6f5df2f13dd8e7b06a1a76a4e6358a4d280e003
Coco001 <wsgkk000@163.com>
2017-03-08 10:52:14 +0800

    判断手机是否联网

---
+++ app/src/main/java/cqupt/screenutil/AccessInternet.java
@@ -0,0 +1,23 @@
+package cqupt.screenutil;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 判断手机是否联网.
+ */
+
+public class AccessInternet extends AppCompatActivity{
+    public boolean isConnect() {
+        boolean connected = false;
+
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            connected = networkInfo.isConnected();
+        }
+        return connected;
+    }
+}
