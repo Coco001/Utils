8cd0523c85f703763708f228a0729bbc2549ed2c
Coco001 <wsgkk000@163.com>
2017-03-23 13:35:23 +0800

    手机屏幕的相关信息

--- app/src/main/java/cqupt/myUtils/ScreenUtil.java
+++ app/src/main/java/cqupt/myUtils/ScreenUtil.java
@@ -1,11 +1,64 @@
 package cqupt.myUtils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
 public class ScreenUtil {
+
+    // 获取旧分辨率
+    public static String getOldLcd(Activity activity) {
+        StringBuffer buffer = new StringBuffer();
+        int density = 0;
+        int widthPixels = 0;
+        int heightPixels = 0;
+        DisplayMetrics dm = new DisplayMetrics();
+        if (dm != null) {
+            activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+            density = dm.densityDpi;
+            widthPixels = dm.widthPixels;
+            heightPixels = dm.heightPixels;
+        }
+        buffer.append(heightPixels);
+        buffer.append("*");
+        buffer.append(widthPixels);
+        buffer.append(",");
+        buffer.append(density);
+        buffer.append("dpi");
+        return buffer.toString();
+    }
+
+    // 获取新分辨率
+    public static String getNewLcd(Activity activity) {
+        StringBuffer buffer = new StringBuffer();
+        int widthPixels = 0;
+        int heightPixels = 0;
+        DisplayMetrics dm = new DisplayMetrics();
+        if (dm != null) {
+            activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+            widthPixels = dm.widthPixels;
+            heightPixels = dm.heightPixels;
+        }
+        buffer.append(heightPixels);
+        buffer.append("*");
+        buffer.append(widthPixels);
+        return buffer.toString();
+    }
+
+    // 获取手机屏幕宽高乘积
+    public static int getScreenSize(Activity activity) {
+        int widthPixels = 0;
+        int heightPixels = 0;
+        DisplayMetrics dm = new DisplayMetrics();
+        if (dm != null) {
+            activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+            widthPixels = dm.widthPixels;
+            heightPixels = dm.heightPixels;
+        }
+        return widthPixels * heightPixels;
+    }
 
     /**
      * 获取屏幕相关参数
